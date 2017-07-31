b118d6867b0bcec6e6ab8acffa67ebf663577c9c
Tess Monical <tess.monical@gmail.com>
2017-07-31 17:16:50 -0500

    more progress

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const chalk = require('chalk');
+const volleyball = require('volleyball');
+
 const app = express();
+const log = console.log;
 
+app.use(volleyball);
 
-app.get('/', function(request, result){
-  result.send("Hello world!")
-})
 
 app.listen(3000, function(request, result){
-  console.log('It is running!');
+  log('It is running!');
 });
 
