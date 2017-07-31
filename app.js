be13fd7048e4231165fea7ccae33b69a5547ac8e
Tess Monical <tess.monical@gmail.com>
2017-07-31 17:32:15 -0500

    Template works

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const chalk = require('chalk');
 const volleyball = require('volleyball');
+const nunjucks = require('nunjucks')
 
 const app = express();
 const log = console.log;
+
+nunjucks.configure('views');
+nunjucks.render('index.html', {title: 'Hi there', people: [{name: "Tess"},{name: "Jan"}]}, function(err, output){
+  log(output);
+});
+
 
 app.use(volleyball);
 
